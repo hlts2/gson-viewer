5de30e75c8d9354e172b2b713b21d24f7d97dac3
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-02-18 17:30:22 +0900

    fix

--- cmd/root.go
+++ cmd/root.go
@@ -10,8 +10,15 @@ func NewRootCmd() *cobra.Command {
 	rootCmd := &cobra.Command{
 		Short: "A CLI tool for simple json viewer",
 		Use:   "goson",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmdutil.CheckErr(runRootCmd(cmd, args))
+		},
 	}
 	return rootCmd
+}
+
+func runRootCmd(cmd *cobra.Command, args []string) error {
+	return nil
 }
 
 //Execute run command
