d6439d8057a24f5f7ebf9948760b99f1c5089488
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-02-18 19:30:58 +0900

    fix

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,20 @@
 package cmd
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+
 	cmdutil "github.com/hlts2/goson/cmd/util"
 	"github.com/spf13/cobra"
+)
+
+var (
+	selectKey string
+
+	rootCmdUsageTemplate = ``
 )
 
 //NewRootCmd returns Command pointer
@@ -14,10 +26,37 @@ func NewRootCmd() *cobra.Command {
 			cmdutil.CheckErr(runRootCmd(cmd, args))
 		},
 	}
+
+	rootCmd.PersistentFlags().StringVarP(&selectKey, "select", "s", "", "set json key to extract json value")
 	return rootCmd
 }
 
 func runRootCmd(cmd *cobra.Command, args []string) error {
+	if len(args) < 1 {
+		return errors.New(rootCmdUsageTemplate)
+	}
+
+	jsonData, err := ioutil.ReadFile(args[0])
+	if err != nil {
+		return err
+	}
+
+	jsonObj := []map[string]interface{}{}
+	err = json.Unmarshal(jsonData, &jsonObj)
+	if err != nil {
+		return err
+	}
+
+	if len(selectKey) == 0 {
+		jsonData, _ := json.Marshal(jsonObj)
+
+		var buf bytes.Buffer
+		json.Indent(&buf, jsonData, "", "  ")
+
+		fmt.Println(buf.String())
+		return nil
+	}
+
 	return nil
 }
 
