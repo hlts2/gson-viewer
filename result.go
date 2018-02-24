5170032fc2256fb50bb76c845e3253f933e7eccb
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-02-24 14:02:37 +0900

    fix

--- result.go
+++ result.go
@@ -4,3 +4,8 @@ package goson
 type Result struct {
 	jsonObject interface{}
 }
+
+//Indent converts json object to json string
+func (r *Result) Indent(prefix, indent string) (string, error) {
+	return indentJSONString(r.jsonObject, prefix, indent)
+}
