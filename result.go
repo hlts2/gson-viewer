ee27f415fb35c002f2e046e4052cb06a0cca9503
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-02-25 15:54:49 +0900

    fix

--- result.go
+++ result.go
@@ -41,13 +41,25 @@ func (r *Result) Int() (int, error) {
 	}
 }
 
-// Int64 converts an interface{} to a int64 and returns an error if types don't match.
-func (r *Result) Int64() (int64, error) {
-	const fn = "Int64"
+// Int8 converts an interface{} to a int8 and returns an error if types don't match.
+func (r *Result) Int8() (int8, error) {
+	const fn = "int8"
 
 	switch r.object.(type) {
-	case int64:
-		return r.object.(int64), nil
+	case int8:
+		return r.object.(int8), nil
+	default:
+		return 0, &ResultError{fn, r.object, ErrorInvalidSyntax}
+	}
+}
+
+// Int16 converts an interface{} to a int16 and returns an error if types don't match.
+func (r *Result) Int16() (int16, error) {
+	const fn = "int16"
+
+	switch r.object.(type) {
+	case int16:
+		return r.object.(int16), nil
 	default:
 		return 0, &ResultError{fn, r.object, ErrorInvalidSyntax}
 	}
@@ -65,6 +77,31 @@ func (r *Result) Int32() (int32, error) {
 	}
 }
 
+// Int64 converts an interface{} to a int64 and returns an error if types don't match.
+func (r *Result) Int64() (int64, error) {
+	const fn = "Int64"
+
+	switch r.object.(type) {
+	case int64:
+		return r.object.(int64), nil
+	default:
+		return 0, &ResultError{fn, r.object, ErrorInvalidSyntax}
+	}
+}
+
+// float32 converts an interface{} to a float32 and returns an error if types don't match.
+func (r *Result) float32() (float32, error) {
+	const fn = "float32"
+
+	switch r.object.(type) {
+	case float32:
+		return r.object.(float32), nil
+	default:
+		return 0, &ResultError{fn, r.object, ErrorInvalidSyntax}
+	}
+}
+
+// float64 converts an interface{} to a float64 and returns an error if types don't match.
 func (r *Result) float64() (float64, error) {
 	const fn = "float64"
 
