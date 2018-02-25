f908e25867371f6b55a6bf227353fccfc5cef3d0
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-02-25 13:50:13 +0900

    fix

--- errors.go
+++ errors.go
@@ -4,15 +4,15 @@ import "errors"
 
 var (
 
-	// ErrorIndexOutOfRange is index out of range error
+	// ErrorIndexOutOfRange represents index out of range error
 	ErrorIndexOutOfRange = errors.New("index out of range")
 
-	// ErrorNotArray is error that target object is not array
+	// ErrorNotArray represents error that target object is not array
 	ErrorNotArray = errors.New("not array")
 
-	// ErrorNotMap is error that target object is not map
+	// ErrorNotMap represents error that target object is not map
 	ErrorNotMap = errors.New("not map")
 
-	// ErrorInvalidJSONKey is error that json path dose not exist
-	ErrorInvalidJSONKey = errors.New("invalid json path")
+	// ErrorInvalidJSONKey represents error that json key dose not exist
+	ErrorInvalidJSONKey = errors.New("invalid json Key")
 )
--- result.go
+++ result.go
@@ -1,17 +1,54 @@
 package goson
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
-// Result is search and path result struct
+//ErrorInvalidSyntax represents invaild syntax error
+var ErrorInvalidSyntax = errors.New("invalid syntax")
+
+//ResultError represents a conversion error
+type ResultError struct {
+	Fn     string
+	Object interface{}
+	Err    error
+}
+
+func (e *ResultError) Error() string {
+	return "goson." + e.Fn + ": parsing: " + Quote(e.Object) + ": " + e.Err.Error()
+}
+
+//Quote returns quoted value
+func Quote(object interface{}) string {
+	return fmt.Sprintf("\"%v\"", object)
+}
+
+// Result represents a json value that is returned from Search() and Path().
 type Result struct {
 	object interface{}
 }
 
-// Int converts search and path result object to int
+// Int converts an interface{} to a int and returns an error if types don't match.
 func (r *Result) Int() (int, error) {
-	v, ok := r.object.(int)
-	if !ok {
-		return 0, errors.New("")
+	const fn = "Int"
+
+	switch r.object.(type) {
+	case int:
+		return r.object.(int), nil
+	default:
+		return 0, &ResultError{fn, r.object, ErrorInvalidSyntax}
 	}
-	return v, nil
+}
+
+// Int64 converts an interface{} to a int64 and returns an error if types don't match.
+func (r *Result) Int64() (int64, error) {
+	const fn = "Int64"
+
+	switch r.object.(type) {
+	case int64:
+		return r.object.(int64), nil
+	default:
+		return 0, &ResultError{fn, r.object, ErrorInvalidSyntax}
+	}
 }
