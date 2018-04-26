d5b510a0c5b681d1c59bd61b9700498f4c3b7824
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-04-26 17:51:00 +0900

    added execute root command process

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "gson-viewer",
 	Short: "A CLI tool to view JSON",
-	Run: func(cmd *cobra.Command, args []string) {
-	},
+}
+
+// Execute execute gson-viewer
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
